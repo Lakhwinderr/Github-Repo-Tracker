1e23c9b9a94611e2dfc2682a4c86de9963717f70
Lakhwinder <lakhwinder.code@gmail.com>
2025-07-18 21:01:06 +0530

    bug solved: the toggle was not working cos the filtered data was indexing new data from 0 again; Solution: used an id key inside the data to reference the data.

--- getAndModifyData.js
+++ getAndModifyData.js
@@ -2,8 +2,9 @@ export async function getData() {
   const response = await fetch("output.json");
   const data = await response.json();
 
-  return data.map(obj => ({
+  return data.map((obj, id) => ({
     ...obj,
+    id: id, // adding an id property for easier reference
     isYours: false,
     hasCleanCode: false,
     hasUpdatedMd: false
